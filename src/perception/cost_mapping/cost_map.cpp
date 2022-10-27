500c25147232a58b511ec0d643ec2d9c0c2de60e
Andrew <arschall@umich.edu>
2022-10-27 19:32:07 -0400

    continued work on ROS node

--- src/perception/cost_mapping/cost_map.cpp
+++ src/perception/cost_mapping/cost_map.cpp
@@ -1,7 +1,7 @@
 #include "cost_map.hpp"
 
 
-
+// calculation should be here
 void CostMapNode::pointCloudCallback(sensor_msgs::PointCloud2ConstPtr const& msg) {
 
 }
@@ -11,8 +11,9 @@ CostMapNode::CostMapNode() : mNh(), mPnh("~"), mTfListener(mTfBuffer) {
     mPnh.param<bool>("publish_cost_maps", mPublishCostMaps, true);
     mPnh.param<bool>("verbose", mIsVerbose, false);
 
-    mPcSub = mNh.subscribe("camera/depth/points", 1, &CostMapNode::pointCloudCallback, this);
+    // TODO: need to advertise cost maps
 
+    mPcSub = mNh.subscribe("camera/depth/points", 1, &CostMapNode::pointCloudCallback, this);
 
     ROS_INFO("Cost map ready");
 }

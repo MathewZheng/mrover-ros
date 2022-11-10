f2a7688f303614319f438c7df277e8b5456f786d
qhdwight <qhdwight@gmail.com>
2022-11-09 19:11:59 -0500

    Add more boilerplate

--- src/perception/cost_mapping/cost_map_processing.cpp
+++ src/perception/cost_mapping/cost_map_processing.cpp
@@ -16,71 +16,58 @@ void CostMapNode::pointCloudCallback(sensor_msgs::PointCloud2ConstPtr const &msg
     // iterate w these, publish into nx, ny, nz
     uint32_t mHeight = 64, mWidth = 64;
 
-    // 1. Get the transformation from the previous rover pose to the current pose
-
-    // 2. Remove all points that are now too far away
-
-    // 3. Use that transform to update all points in mCostMapPoints, the "point" vector
-
-    // 4. Iterate mCloudPtr and add all new cost map calculations to mCostMapPoints DONE
-
-    // 5. For each point in mCostMapPoints, add to its correct "bin" or cell DONE
-
-    // 6. There is a chance there will be multiple, keep ony the one with the newest "frameNumberSeen"
-
-    // 7. Actually create the cost map and put it into mLocalGrid, publish it
-
     geometry_msgs::TransformStamped pcTf = mTfBuffer.lookupTransform("map", "base_link", ros::Time(0));
     // fromTf is still erroring (something about it being inaccessible)
-    SE3 SE3Tf = SE3::fromTf(pcTf.transform);
+    // TODO: calculate delta using last two poses (quintin: I can provide this)
     SE3 delta;
-    // TODO: Calculate transform using SE3
 
-    float minx = mCloudPtr->at(0).x;
-    float miny = mCloudPtr->at(0).y;
+    // Clear scratch buffer
+    for (auto &row: mCostMapPointsScratch) {
+        for (auto &cost: row) {
+            cost = std::nullopt;
+        }
+    }
+
+    // Transform all current points
+    for (size_t i = 0; i < mCostMapPoints.size(); ++i) {
+        for (size_t j = 0; i < mCostMapPoints[i].size(); ++j) {
+            std::pair<size_t, size_t> currentPoint{i, j};
+
+            // TODO: fill mCostMapPointsScratch in with transformed point with delta
+            // TODO: don't fill in points that now lie outside
+        }
+    }
+
+    // Add/replace new point cloud points
     pcl::fromROSMsg(*msg, *mCloudPtr);
     for (pcl::PointXYZRGBNormal &point: *mCloudPtr) {
         Eigen::Vector2f xy{point.x, point.y};
         Eigen::Vector3f normal{point.normal_x, point.normal_y, 0.0f};
         Eigen::Vector3f up{0.0f, 0.0f, 1.0f};
-        float cost = 1.0f - std::fabs(normal.dot(up));
-        auto intCost = static_cast<uint8_t>(std::lround(cost) * std::numeric_limits<uint8_t>::max());
 
-        
-        // check here maybe to see if it exists already in costMap
-        std::pair<int, int> currentPoint = convertToCell(xy);
-        for (int i = 0; i < (int) mCostMapPoints.size(); ++i) {
-            std::pair<int, int> costMapPoint = convertToCell(mCostMapPoints[i].point.x(), mCostMapPoints[i].point.y());
-            if (currentPoint == costMapPoint) {
-                mCostMapPoints.erase(mCostMapPoints.begin() + i);
-                break;
-            }
-        }
-        mCostMapPoints.emplace_back(Eigen::Vector2f{point.x, point.y}, intCost, mFrameNumber);
-        if (point.x < minx) {
-            minx = point.x;
-        }
-        if (point.y < miny) {
-            miny = point.y;
+        float costValue = 1.0f - std::fabs(normal.dot(up));
+        auto intCostValue = static_cast<uint8_t>(std::lround(costValue) * std::numeric_limits<uint8_t>::max());
+        std::pair<size_t, size_t> currentCell = convertToCell(xy);
+
+        auto &cost = mCostMapPoints[currentCell.first][currentCell.second];
+        if (cost) {
+            cost->cost = intCostValue;
+            cost->point = xy;
         }
     }
 
-    // Should be done now: Change this to use resolution to calculate indices
-    // Idea: for checking duplicates potentially keep track with a set?
-    for (size_t i = 0; i < mCostMapPoints.size(); ++i) {
-        std::pair<int, int> currentPoint = convertToCell(mCostMapPoints[i].point.x(), mCostMapPoints[i].point.y());
-    }
+    std::swap(mCostMapPoints, mCostMapPointsScratch);
+
+    // TODO: put from mCostMapPoints into mLocalGrid
 
     if (mPublishCostMaps) {
         mCostMapPub.publish(mLocalGrid);
     }
 
     mFrameNumber++;
-
-    std::swap(mCostMapPoints, mCostMapPointsScratch);
 }
 
-std::pair<size_t, size_t> CostMapNode::convertToCell(Eigen::Vector2f const& point) {
+std::pair<size_t, size_t> CostMapNode::convertToCell(Eigen::Vector2f const &point) {
     // HERE: code for the previous way we calculated the indices
     // float differencex = pointx - minx;
     // float differencey = pointy - miny;

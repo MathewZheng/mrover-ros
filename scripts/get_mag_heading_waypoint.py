76b7e2f0a216a499a825c39a57d658ddac9cdf71
alisonryckman <alison.ryckman@gmail.com>
2023-08-08 00:26:20 -0400

    fixed bugs so it runs

--- scripts/get_mag_heading_waypoint.py
+++ scripts/get_mag_heading_waypoint.py
@@ -4,15 +4,16 @@ from util.tf_utils import EARTH_RADIUS
 
 from math import asin, atan2, sin, cos
 
-def main():
-    deg = input("Compass Heading Degrees: ")
-    min = input("Compass Heading Minutes (\"): ")
-    sec = input("Compass Heading Seconds (\'): ")
-    dist = input("Distance (m): ")
+def calc_final_waypoint():
+    deg = float(input("Compass Heading Degrees: "))
+    min = float(input("Compass Heading Minutes (\"): "))
+    sec = float(input("Compass Heading Seconds (\'): "))
+    dist = float(input("Distance (m): "))
 
     theta = deg + min / 60 + sec / 3600
 
-    waypoint = rospy.wait_for_message('/gps/fix', NavSatFix, timeout=5)
+    waypoint = rospy.wait_for_message("gps/fix", NavSatFix, timeout=5)
+
     long_i = waypoint.longitude
     lat_i = waypoint.latitude
     delta = dist / EARTH_RADIUS
@@ -20,5 +21,12 @@ def main():
     # source: https://www.movable-type.co.uk/scripts/latlong.html
     lat_f = asin(sin(lat_i) * cos(delta) + cos(lat_i) * sin(delta) * cos(theta))
     long_f = long_i + atan2(sin(theta) * sin(delta) * cos(lat_i), cos(delta) - sin(lat_i) * sin(lat_f))
-    print("longitude: " + long_f + " N")
-    print("latitude: " + lat_f + " E")
+    print("longitude: " + str(long_f) + " N")
+    print("latitude: " + str(lat_f) + " E")
+
+def main():
+    rospy.init_node("heading_waypoint")
+    calc_final_waypoint()
+
+if __name__ == "__main__":
+    main()
